ab14d4fa2f483305519bb71d8dad3fb2099742b6
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-04-23 11:39:24 +0530

    Bind size and cuisine combo

--- Fast Food Restaurant/App.cs
+++ Fast Food Restaurant/App.cs
@@ -25,6 +25,8 @@ namespace Fast_Food_Restaurant
         private void App_Load(object sender, EventArgs e)
         {
             bindFoodData();
+            bindSizeCombo();
+            bindCuisineCombo();
         }
 
         DataTable food_dt = new DataTable();
@@ -34,38 +36,103 @@ namespace Fast_Food_Restaurant
             foodTable.DataSource = null;
             foodTable.Rows.Clear();
             foodTable.Refresh();
-            // view_timetable_table.CellClick += view_timetable_table_CellClick;
-            string query = "select * from Food";
-            using(SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(connectionString))
             {
-                SqlCommand cmd = new SqlCommand(query, con);
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                sda.Fill(food_dt);
+                try
+                {
+                    string query = "SELECT * FROM Food";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            sda.Fill(food_dt);
 
-                foodTable.AutoGenerateColumns = false;
-                foodTable.ColumnCount = 4;
+                            foodTable.AutoGenerateColumns = false;
+                            foodTable.ColumnCount = 4;
 
-                //Add Columns
-                foodTable.Columns[0].Name = "foodid";
-                foodTable.Columns[0].HeaderText = "FoodID";
-                foodTable.Columns[0].DataPropertyName = "FoodID";
+                            //Add Columns
+                            foodTable.Columns[0].Name = "foodid";
+                            foodTable.Columns[0].HeaderText = "FoodID";
+                            foodTable.Columns[0].DataPropertyName = "FoodID";
 
-                foodTable.Columns[1].HeaderText = "foodname";
-                foodTable.Columns[1].Name = "FoodName";
-                foodTable.Columns[1].DataPropertyName = "FoodName";
+                            foodTable.Columns[1].HeaderText = "foodname";
+                            foodTable.Columns[1].Name = "FoodName";
+                            foodTable.Columns[1].DataPropertyName = "FoodName";
 
-                foodTable.Columns[2].Name = "price";
-                foodTable.Columns[2].HeaderText = "Price";
-                foodTable.Columns[2].DataPropertyName = "Price";
+                            foodTable.Columns[2].Name = "price";
+                            foodTable.Columns[2].HeaderText = "Price";
+                            foodTable.Columns[2].DataPropertyName = "Price";
 
-                foodTable.Columns[3].Name = "description";
-                foodTable.Columns[3].HeaderText = "Description";
-                foodTable.Columns[3].DataPropertyName = "Description";
+                            foodTable.Columns[3].Name = "description";
+                            foodTable.Columns[3].HeaderText = "Description";
+                            foodTable.Columns[3].DataPropertyName = "Description";
 
-                foodTable.DataSource = food_dt;    
+                            foodTable.DataSource = food_dt;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }     
+            }
+            foodTable.ClearSelection();
+        }
+
+        private void bindSizeCombo()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT SizeName FROM MealSize";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                sizecomboBox.Items.Add(dr[0]);
+                            }
+                        }
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
             }
         }
 
+        private void bindCuisineCombo()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT CuisineName FROM Cuisine";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                cuisinecomboBox.Items.Add(dr[0]);
+                            }
+                        }
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+        
         private void foodTable_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if(e.RowIndex >=0)
@@ -75,10 +142,7 @@ namespace Fast_Food_Restaurant
                 foodNameTextBox.Text = row.Cells["FoodName"].Value.ToString();
                 priceTextBox.Text = row.Cells["Price"].Value.ToString();
                 descriptionTextBox.Text = row.Cells["Description"].Value.ToString();
-                
-
-            }
-            
+            }      
         }
     }
 }
